7936dd1e27d6b59efa05cd6677bca8136e443a6e
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-01-26 17:46:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import numpy as np
 
 st.write("""
 Hello Leo,
-Dnes již tři týdny se známe,
-Bylo to velice krásné, příjemné a zrádné,
-Po tobě bude se mi teď stýskati,
-Doufám, že ve snech tvou krásu budu vídati,
-Nechť mají se tvé oči krásně,
+Dnes již tři týdny se známe,\\
+Bylo to velice krásné, příjemné a zrádné,\\
+Po tobě bude se mi teď stýskati,\\
+Doufám, že ve snech tvou krásu budu vídati,\\
+Nechť mají se tvé oči krásně,\\
 Těším se až uvidím tě jasně.
 """)
