d823bb7b33a0404c9ffcc084890faa56bdb4fe36
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-01-27 01:17:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,4 @@ Těším se až uvidím tě jasně.
 df = pd.read_csv('./doctor_prescription_counts.csv')
 st.write(df)
 
-df = pd.read_csv('./medication_usage_summary.csv')
-bins = [0, 5, 10, 15, 20, float('inf')]
-labels = ['1-5', '6-10', '11-15', '16-20', '20+']
-df['category'] = pd.cut(df['user_count'], bins=bins, labels=labels, right=False)
 
-category_counts = df['category'].value_counts(normalize=True) * 100
-
-
-fig, ax = plt.subplots(figsize=(10, 6))
-category_counts.plot(kind='bar', ax=ax)
-ax.set_xlabel('Number of Users')
-ax.set_ylabel('Percentage of Medications')
-ax.set_title('Medication Usage by User Count Categories')
-fig = fig.figure
-st.pyplot(fig)
