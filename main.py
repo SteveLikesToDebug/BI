90941a490142abeeaf8577ca9a705791e4b047e3
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-01-27 01:35:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,34 @@ import numpy as np
 
 
 st.write("""
-Hello Leo,\\
-Dnes již tři týdny se známe,\\
-Bylo to velice krásné, příjemné a zrádné,\\
-Po tobě bude se mi teď stýskati,\\
-Doufám, že ve snech tvou krásu budu vídati,\\
-Nechť mají se tvé oči krásně,\\
-Těším se až uvidím tě jasně.
-""")
+    Hello Leo,\\
+    Dnes již tři týdny se známe,\\
+    Bylo to velice krásné, příjemné a zrádné,\\
+    Po tobě bude se mi teď stýskati,\\
+    Doufám, že ve snech tvou krásu budu vídati,\\
+    Nechť mají se tvé oči krásně,\\
+    Těším se až uvidím tě jasně.
+    """)
 
 
-df = pd.read_csv('./doctor_prescription_counts.csv')
+df = pd.read_csv('./doctor_prescription_counts.csv', sep=';')
 st.write(df)
 
 
 
-import pylab
 import matplotlib.pyplot as plt
-df = pd.read_csv('./medication_usage_summary.csv')
+df2 = pd.read_csv('./medication_usage_summary.csv', sep=';')
 bins = [0, 5, 10, 15, 20, float('inf')]
 labels = ['1-5', '6-10', '11-15', '16-20', '20+']
-df['category'] = pd.cut(df['user_count'], bins=bins, labels=labels, right=False)
+df2['category'] = pd.cut(df2['No. users taking'], bins=bins, labels=labels, right=False)
 
-category_counts = df['category'].value_counts(normalize=True) * 100
-
+category_counts = df2.groupby('category', observed=True)['Medication Name'].count()
 
 fig, ax = plt.subplots(figsize=(10, 6))
 category_counts.plot(kind='bar', ax=ax)
 ax.set_xlabel('Number of Users')
-ax.set_ylabel('Percentage of Medications')
-ax.set_title('Medication Usage by User Count Categories')
-fig = fig.figure
+ax.set_ylabel('Number of Medication Names')
+ax.set_title('Number of Medication Names by User Count Categories')
+
+#plt.show()
 st.pyplot(fig)
