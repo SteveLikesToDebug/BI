2bda6e302fc3b912d925d014a356f57c78dc83f9
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-01-31 18:33:56 +0100

    ah

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def intro():
     st.write("Pro jednotlivé rozebírané kategorie prosím volte z menu vlevo. V každé naleznete rychlou analýzu dat z databáze, jejíž struktura je taktéž k nahlédnutí.")
 
     st.subheader("Databaze léků")
-    medication_data = pd.read_csv('./sukl_meds_all.csv', sep=';')
-    st.dataframe(medication_data)
+    medication_data = pd.read_csv('./sukl_meds_all.csv', sep=';', dtype={'your_column_name': str})
     
+    st.dataframe(medication_data)
+
 
 def medicaments():
     st.title("Volně prodejné léky")
