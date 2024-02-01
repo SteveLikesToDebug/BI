45ef5e9217955823292b826ff6763e61c001cf58
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-02-01 08:12:27 +0100

    ah

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def prescriptions():
 
 def database_overview():
     st.write("Zde můžete vidět veškeré věci co se týkají databáze")
-    st.image("./IMG_2488.HEIC", caption='Schéma databáze')
+    st.image("./IMG_2488.jpg", caption='Schéma databáze')
 def basnicka():
     st.title('Pro Leu')
     st.write("""
