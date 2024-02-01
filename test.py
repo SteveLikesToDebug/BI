1a3a62a8c11c0417f3753708f05f2088855bf5e7
SteveLikesToDebug <116626552+SteveLikesToDebug@users.noreply.github.com>
2024-02-01 08:27:45 +0100

    ah

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 try:
-    img = Image.open('IMG_2488.png')
-    img.show()  # This should open the image in your default image viewer
+    img = Image.open('/Users/stepan/Desktop/BI/github/IMG_2488.png')
+    img.show()
 except IOError:
     print("Error: Cannot open the image.")
